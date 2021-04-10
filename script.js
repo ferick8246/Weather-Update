190555b5e23737f0eaffb32a8825d568478ea364
Erick Flore <ferick8246@gmail.com>
2021-04-10 00:12:46 -0500

    HTTPS

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ uvEl.appendChild(buttonEl);
 };
 
 function searchWeather(searchValue) {
- var endpoint = (`http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial`);
+ var endpoint = (`https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial`);
  fetch(endpoint)
  .then((res) => res.json())
  .then((data) => {
