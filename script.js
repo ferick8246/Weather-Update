e80b31349d4c3a3d4e63d31c5884b9556986b3f2
Erick Flore <ferick8246@gmail.com>
2021-04-09 21:43:07 -0500

    Function to get the forecast to loop

--- script.js
+++ script.js
@@ -5,7 +5,20 @@ window.addEventListener('load', function () {
     } else {
       existingHistory = JSON.parse(localStorage.getItem('history'));
     }
-  
+
+  var historyItems = [];
+
+  function getForecast(searchValue) {
+    if (!searchValue) {
+      return;
+    }
+    var endpoint = `http://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial`;
+    fetch(endpoint)
+      .then((res) => res.json())
+      .then((data) => {
+        // Select our forecast element and add a header to it
+        var forecastEl = document.querySelector('#forecast');
+        forecastEl.innerHTML = '<h4 class="mt-3">5-Day Forecast:</h4>';
     
 
     
