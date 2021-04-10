0c9016f725f912fc874cd2f39355e7f576d25e11
Erick Flore <ferick8246@gmail.com>
2021-04-09 21:57:54 -0500

    Created HTML elements for bootstrap

--- script.js
+++ script.js
@@ -16,12 +16,41 @@ window.addEventListener('load', function () {
     fetch(endpoint)
       .then((res) => res.json())
       .then((data) => {
-        // Select our forecast element
-        var forecastEl = document.querySelector('#forecast');
+       
+    var forecastEl = document.querySelector('#forecast');
         forecastEl.innerHTML = '<h4 class="mt-3">5-Day Forecast:</h4>';
 
-        forecastRowEl = document.createElement('div');
-        forecastRowEl.className = '"row"';
+   forecastRowEl = document.createElement('div');
+   forecastRowEl.className = '"row"';
+    for (var i = 0; i < data.list.length; i++) {
+    if (data.list[i].dt_txt.indexOf('15:00:00') !== -1) {
+    var colEl = document.createElement('div');
+    colEl.classList.add('col-md-2');
+    var cardEl = document.createElement('div');
+    cardEl.classList.add('card', 'bg-primary', 'text-white');
+    var windEl = document.createElement('p');
+    windEl.classList.add('card-text');
+    windEl.textContent = `Wind Speed: ${data.list[i].wind.speed} MPH`;
+    var humidityEl = document.createElement('p');
+    humidityEl.classList.add('card-text');
+    humidityEl.textContent = `Humidity : ${data.list[i].main.humidity} %`;
+    var bodyEl = document.createElement('div');
+    bodyEl.classList.add('card-body', 'p-2');
+    var jotoQueLea = document.createElement('h5');
+    jotoQueLea.classList.add('card-title');
+    jotoQueLea.textContent = new Date(
+    data.list[i].dt_txt
+    ).toLocaleDateString();
+    var imgEl = document.createElement('img');
+    imgEl.setAttribute(
+    'src',
+    `http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`);
+    var p1El = document.createElement('p');
+    p1El.classList.add('card-text');
+    p1El.textContent = `Temp: ${data.list[i].main.temp_max} °F`;
+    var p2El = document.createElement('p');
+    p2El.classList.add('card-text');
+    p2El.textContent = `Humidity: ${data.list[i].main.humidity}%`;
     
 
     
